8ef314077eb1938434eed5d6cc49e72ec8ebe849
lovingstr <68695677+lovingstr@users.noreply.github.com>
2021-07-30 22:21:16 +0800

    Add files via upload

---
+++ NH4HCO3.py
@@ -0,0 +1,13 @@
+from random import shuffle
+n = 6 # 人数
+list1 = [x for x in range(1, n + 1)]
+
+list_people = ["狼", "狼", "预言", "猎人", "女巫", "白狼"] # 神职
+shuffle(list_people)
+
+result = {}
+for x in range(n):
+    result[list1[x]] = list_people[x]
+
+print(result)
+
